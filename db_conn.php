179f9747a3498c989e25f4dfee15a9d8cfb8490c
TrixDiaz <john.darlucio022@gmail.com>
2023-04-27 19:24:25 +0800

    integ

--- db_conn.php
+++ db_conn.php
@@ -4,9 +4,9 @@ $name = "localhost";
 $uname = "root";
 $password = "";
 
-$db_name = "database";
+$db_name = "laravel_integration";
 $conn = mysqli_connect($sname, $uname, $password, $dbname);
 
 if(!$conn){
     echo "Connection failed";
-} ?>
+}
